24c48da89f3c0b8d9a84cd8c3345f4f327fcabcb
xkunD <103330637+xkunD@users.noreply.github.com>
2024-03-28 23:07:04 -0400

    pa1 first 5 route

--- pa1/src/app.py
+++ pa1/src/app.py
@@ -6,7 +6,57 @@ from flask import request
  
 app = Flask(__name__)
 
-task_id_counter = 2
+post_id_counter = 2
+
+posts = {
+    0: {"id": 0,
+        "upvotes": 1,
+        "title": "My cat is the cutest!",
+        "link": "https://i.imgur.com/jseZqNK.jpg",
+        "username": "alicia98",},
+    1: {"id": 1,
+        "upvotes": 3,
+        "title": "Cat loaf",
+        "link": "https://i.imgur.com/TJ46wX4.jpg",
+        "username": "alicia98"},
+}
+
+
+@app.route("/api/posts/")
+def get_posts():
+    res = {"posts" : list(posts.values())}
+    return json.dumps(res), 200
+
+
+@app.route("/api/posts/", methods = ["POST"])
+def create_post():
+    global post_id_counter
+    body = json.loads(request.data)
+    description = body.get("description")
+    post = {
+        "id": post_id_counter,
+        "description": description,
+        "done": False
+    }
+    posts[post_id_counter] = post
+    post_id_counter += 1
+    return json.dumps(post), 201
+
+@app.route("/api/posts/<int:task_id>/")
+def get_post(post_id):
+    post = posts.get(post_id)
+    if not post:
+        return json.dumps({"error": "Post not found"}), 404
+    return json.dumps(post), 200
+
+@app.route("/api/posts/<int:task_id>/", methods = ["DELETE"])
+def delete_task(task_id):
+    task = posts.get(task_id)
+    if not task:
+        return json.dumps({"error": "Task not found"}), 404
+    del posts[task_id]
+    return json.dumps(task), 200
+
 
 
 @app.route("/")
