70bdd31dad10e9daaa01af20ac858ae071c2a4b5
xkunD <103330637+xkunD@users.noreply.github.com>
2024-04-09 19:01:01 -0400

    create task table

--- pa2_demo/src/db.py
+++ pa2_demo/src/db.py
@@ -20,7 +20,28 @@ class DatabaseDriver(object):
     """
 
     def __init__(self):
-        pass
+        """
+        Secures a connection with the database and stores it into the instance variable conn.
+        """
+        self.conn = sqlite3.connect("todo.db", check_same_thread=False)
+        
+    def create_task_table(self):
+        '''
+        Using SQL, creates a task table
+        '''
+        self.conn.execute("""
+        CREATE TABLE IF NOT EXIST task(
+                          id INTEGER PRIMARY KEY AUTOINCREMENT
+                          description TEXT NOT NULL
+                          done BOOLEAN NOT NULL
+        );""")
+    
+    def delete_task_table(self):
+        """
+        Using SQL, delete a task table
+        """
+        self.conn.execute("""
+""")
 
 
 # Only <=1 instance of the database driver
