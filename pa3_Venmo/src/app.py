d0b8e602533c62d4b173497e9aea49b0433dedef
xkunD <103330637+xkunD@users.noreply.github.com>
2024-04-17 21:14:09 -0400

    copied from pa2

--- pa3_Venmo/src/app.py
+++ pa3_Venmo/src/app.py
@@ -1,9 +1,6 @@
-from datetime import datetime
 import json
-
+from flask import Flask, request
 import db
-from flask import Flask
-from flask import request
 
 DB = db.DatabaseDriver()
 
@@ -14,8 +11,75 @@ app = Flask(__name__)
 def hello_world():
     return "Hello world!"
 
+@app.route("/api/users/")
+def get_users():
+    """
+    Endpoint for getting all users
+    """
+    return json.dumps({"users": DB.get_all_users()}),200
 
-# your routes here
+@app.route("/api/users/", methods=["POST"])
+def create_user():
+    """
+    Endpoint for creating a user
+    """
+    body = json.loads(request.data)
+    name = body.get("name")
+    username = body.get("username")
+    balance = body.get("balance", 0)
+    user_id = DB.insert_user_table(name, username, balance)
+    user = DB.get_user_by_id(user_id)
+    if user is None:
+        return json.dumps({"error": "User not found"}), 404
+    return json.dumps(user), 201
+
+@app.route("/api/user/<int:user_id>/")
+def get_user(user_id):
+    """
+    Endpoint for getting a user by its ID
+    """
+    user = DB.get_user_by_id(user_id)
+    if user is None:
+        return json.dumps({"error": "User not found"}), 404
+    return json.dumps(user), 200
+
+@app.route("/api/user/<int:user_id>/", methods=["DELETE"])
+def delete_user(user_id):
+    """
+    Endpoint for deleting a user by his ID
+    """
+    user = DB.get_user_by_id(user_id)
+    if user is None:
+        return json.dumps({"error": "User not found"}), 404
+    DB.delete_user_by_id(user_id)
+    return json.dumps(user), 200
+
+@app.route("/api/send/", methods=["POST"])
+def send_money():
+    """
+    Send money from one user to another
+    """
+    body = json.loads(request.data)
+    sender_id = body.get("sender_id")
+    receiver_id = body.get("receiver_id")
+    amount = body.get("amount")
+    if sender_id is None or receiver_id is None or amount is None:
+        return json.dumps({"error": "Invalid Input"}), 400
+    sender_balance = DB.get_balance_by_id(sender_id)
+    receiver_balance = DB.get_balance_by_id(receiver_id)
+    if sender_balance is None or receiver_balance is None:
+        return json.dumps({"error": "User not found"}), 400
+    if sender_balance < amount:
+        return json.dumps({"error": "User balance overdraft"}), 400
+    sender_balance -= amount
+    receiver_balance += amount
+    DB.update_balance_by_id(sender_balance, sender_id)
+    DB.update_balance_by_id(receiver_balance, receiver_id)
+    return json.dumps({"sender_id": sender_id,
+    "receiver_id": receiver_id,
+    "amount": amount}), 200
+
+
 
 
 if __name__ == "__main__":
--- pa3_Venmo/src/db.py
+++ pa3_Venmo/src/db.py
@@ -1,3 +1,4 @@
+import os
 import sqlite3
 
 # From: https://goo.gl/YzypOI
@@ -14,12 +15,90 @@ def singleton(cls):
 
 class DatabaseDriver(object):
     """
-    Database driver for the Venmo (Full) app.
+    Database driver for the app.
     Handles with reading and writing data with the database.
     """
 
     def __init__(self):
-        pass
+        """
+        Secures a connection with the database and stores it into the instance variable conn.
+        """
+        self.conn = sqlite3.connect("user.db", check_same_thread=False)
+        self.create_user_table()
+
+    def create_user_table(self):
+        '''
+        Using SQL, creates a user table
+        '''
+        self.conn.execute("""
+        CREATE TABLE IF NOT EXISTS user(
+                          id INTEGER PRIMARY KEY AUTOINCREMENT,
+                          name TEXT NOT NULL,
+                          username TEXT NOT NULL,
+                          balance INTEGER NOT NULL);""")
+
+    def delete_user_table(self):
+        """
+        Using SQL, delete a user table
+        """
+        self.conn.execute("""
+        DROP TABLE IF EXISTS user;
+    """)
+        
+    def get_all_users(self):
+        """
+        Using SQL, return all the users in a table
+        """
+        cursor = self.conn.execute("SELECT * FROM user;")
+        users = []
+        for row in cursor:
+            users.append({"id": row[0], "name": row[1], "username": row[2]})
+        return users
+    
+    def insert_user_table(self, name, username, balance):
+        '''
+        Using SQL, inserts a user into a user table
+        '''
+        cursor = self.conn.execute("""
+                                   INSERT INTO user(name, username, balance) VALUES (?, ?, ?);""", (name, username, balance))
+        self.conn.commit()
+        return cursor.lastrowid
+    
+    def get_user_by_id(self, user_id):
+        """
+        Using SQL, get a user by his ID
+        """
+        cursor = self.conn.execute("SELECT * FROM user WHERE id = ?;", (user_id,))
+        for row in cursor:
+            return ({"id": row[0], "name": row[1], "username": row[2], "balance": row[3]})
+        return None        
+
+    def delete_user_by_id(self, id):
+        """
+        Using SQL, delete a user by his ID
+        """
+        self.conn.execute("""
+                          DELETE FROM user WHERE id=?""", (id,))
+
+    def get_balance_by_id(self, user_id):
+        """
+        Using SQL, get a user's balance by his iD
+        """
+        cursor = self.conn.execute("SELECT * FROM user WHERE id = ?;", (user_id,))
+        for row in cursor:
+            return row[3]
+        return None 
+
+    def update_balance_by_id(self, balance, id):
+        """
+        Using SQL, update the balance of a user
+        """
+        self.conn.execute("""
+                          UPDATE user SET balance = ? WHERE id = ?""", (balance, id))
+        self.conn.commit()     
+        
+
+
 
 # Only <=1 instance of the database driver
 # exists within the app at all times
