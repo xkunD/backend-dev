f8b88d6b36d35a50d0109c7d08864e48be731dca
xkunD <103330637+xkunD@users.noreply.github.com>
2024-04-17 21:11:37 -0400

    add starter file for pa3

---
+++ pa3_Venmo/src/app.py
@@ -0,0 +1,22 @@
+from datetime import datetime
+import json
+
+import db
+from flask import Flask
+from flask import request
+
+DB = db.DatabaseDriver()
+
+app = Flask(__name__)
+
+
+@app.route("/")
+def hello_world():
+    return "Hello world!"
+
+
+# your routes here
+
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=8000, debug=True)
