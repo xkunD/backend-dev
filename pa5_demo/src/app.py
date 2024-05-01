93d24c8d4be5621d3e072643f32340e739f7ed63
xkunD <103330637+xkunD@users.noreply.github.com>
2024-05-01 13:47:08 -0400

    pa5 demo setup

---
+++ pa5_demo/src/app.py
@@ -0,0 +1,152 @@
+import json
+
+from db import db
+from flask import Flask, request
+from db import Task, Subtask,Category
+
+# define db filename
+db_filename = "todo.db"
+app = Flask(__name__)
+
+# setup config
+app.config["SQLALCHEMY_DATABASE_URI"] = f"sqlite:///{db_filename}"
+app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
+app.config["SQLALCHEMY_ECHO"] = True
+
+# initialize app
+db.init_app(app)
+with app.app_context():
+    db.create_all()
+
+
+# generalized response formats
+def success_response(data, code=200):
+    return json.dumps(data), code
+
+
+def failure_response(message, code=404):
+    return json.dumps({"error": message}), code
+
+
+# -- TASK ROUTES ------------------------------------------------------
+
+
+@app.route("/")
+@app.route("/tasks/")
+def get_tasks():
+    """
+    Endpoint for getting all tasks
+    """
+    # tasks = []
+    # for task in Task.query.all():
+    #     tasks.append(task.serialize())
+    # return success_response({"tasks": tasks})
+
+    return success_response({"tasks": [t.serialize() for t in Task.query.all()]})
+        
+
+
+@app.route("/tasks/", methods=["POST"])
+def create_task():
+    """
+    Endpoint for creating a new task
+    """
+    body = json.loads(request.data)
+    new_task = Task(
+        description = body.get('description'),
+        done = body.get("done")
+    )
+    db.session.add(new_task)
+    db.session.commit()
+    return success_response(new_task.serialize(), 201)
+
+
+
+@app.route("/tasks/<int:task_id>/")
+def get_task(task_id):
+    """
+    Endpoint for getting a task by id
+    """
+    task = Task.query.filter_by(id=task_id).first()
+    if task is None:
+        return failure_response("Task not found!")
+    return success_response(task.serialize())
+
+
+
+@app.route("/tasks/<int:task_id>/", methods=["POST"])
+def update_task(task_id):
+    """
+    Endpoint for updating a task by id
+    """
+    body = json.loads(request.data)
+    task = Task.query.filter_by(id=task_id).first()
+    if task is None:
+        return failure_response("Task not found!")
+    task.description = body.get("description")
+    task.done = body.get("done")
+    db.session.commit()
+    return success_response(task.serialize()) 
+
+
+@app.route("/tasks/<int:task_id>/", methods=["DELETE"])
+def delete_task(task_id):
+    """
+    Endpoint for deleting a task by id
+    """
+    task = Task.query.filter_by(id=task_id).first()
+    if task is None:
+        return failure_response("Task not found!")
+    db.session.delete(task)
+    db.session.commit()
+    return success_response(task.serialize())
+
+
+# -- SUBTASK ROUTES ---------------------------------------------------
+
+
+@app.route("/tasks/<int:task_id>/subtasks/", methods=["POST"])
+def create_subtask(task_id):
+    """
+    Endpoint for creating a subtask
+    for a task by id
+    """
+    task = Task.query.filter_by(id = task_id).first()
+    if task is None:
+        return failure_response("Taks not found")
+    body = json.loads(request.data)
+    new_subtask = Subtask(
+        description = body.get("description"),
+        done = body.get("done"),
+        task_id = task_id
+    )
+    db.session.add(new_subtask)
+    db.session.commit()
+    return success_response(new_subtask.serialize(), 201)
+
+
+# -- CATEGORY ROUTES --------------------------------------------------
+
+
+@app.route("/tasks/<int:task_id>/category/", methods=["POST"])
+def assign_category(task_id):
+    """
+    Endpoint for assigning a category
+    to a task by id
+    """
+    task = Task.query.filter_by(id=task_id).first()
+    if task is None:
+        return failure_response("Task not found!")
+    body = json.loads(request.data)
+    description = body.get("description")
+    color = body.get("color")
+    category = Category.query.filter_by(color = color).first()
+    if category is None:
+        category = Category(description = description, color = color)
+    task.categories.append(category)
+    db.session.commit()
+    return success_response(task.serialize())
+
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=8000, debug=True)
---
+++ pa5_demo/src/db.py
@@ -0,0 +1,104 @@
+from flask_sqlalchemy import SQLAlchemy
+
+db = SQLAlchemy()
+
+association_table = db.Table(
+    "association",
+    db.Model.metadata,
+    db.Column("task_id", db.Integer, db.ForeignKey("tasks.id")),
+    db.Column("category_id", db.Integer, db.ForeignKey("categories.id"))
+)
+
+class Task(db.Model):
+    """
+    Task Model
+    """
+    __tablename__ = "tasks"
+    id = db.Column (db.Integer, primary_key=True, autoincrement=True)
+    description = db.Column(db.String, nullable=False)
+    done = db.Column(db.Boolean, nullable=False)
+    subtasks = db.relationship("Subtask", cascade="delete")
+    categories = db.relationship("Category", secondary=association_table, back_populates="tasks")
+
+    def __init__(self, **kwargs):
+        """Initialize Task object/entry"""
+        self.description = kwargs.get("description", "")
+        self.done = kwargs.get ("done", False)
+
+    def serialize(self):
+        """
+        Serialize a task object
+        """
+        return {
+            "id": self.id,
+            "description": self.description,
+            "done": self.done,
+            "subtasks": [s.serialize() for s in self.subtasks],
+            "categories": [c.simple_serialize() for c in self.categories]
+        }
+    
+
+class Subtask(db.Model):
+    """Subtask Model"""
+
+    __tablemane__ = "subtasks"
+    id = db.Column(db.Integer, primary_key=True, autoincrement=True)
+    description = db.Column(db.String, nullable=False)
+    done = db.Column(db.Boolean, nullable=False)
+    task_id = db.Column(db.Integer, db.ForeignKey("tasks.id"),nullable=False)
+
+    def __init__(self, **kwargs) :
+
+        """ 
+        Initialize the subtask obiect
+        """
+        self.description = kwargs.get("description", "")
+        self.done = kwargs.get("done", False)
+        self.task_id = kwargs.get("task_id")
+
+    def serialize(self):
+        """
+        Serialize a subtask object
+        """
+        return{
+            "id": self.id,
+            "description": self.description,
+            "done": self.done,
+            "task_id": self.task_id
+        }
+        
+class Category(db.Model):
+    """
+    Category Model
+    """
+    __tablename__ = "categories"
+    id = db.Column(db.Integer, primary_key=True, autoincrement=True)
+    description = db.Column(db.String, nullable =False)
+    color = db.Column(db.String, nullable=False)
+    tasks = db.relationship("Task",secondary=association_table, back_populates="categories")
+
+    def __init__(self, **kwargs):
+        """
+        Initialize a category object
+        """
+        self.description = kwargs.get("description")
+        self.color = kwargs.get("color")
+
+    def serialize(self):
+        """
+        Serialize a category object
+        """
+        return{
+            "id": self.id,
+            "description": self.description,
+            "color": self.color,
+            "tasks": [t.serialize() for t in self.tasks]
+        }
+    
+    def simple_serialize(self):
+        return{
+            "id": self.id,
+            "description": self.description,
+            "color": self.color,
+        }
+    
